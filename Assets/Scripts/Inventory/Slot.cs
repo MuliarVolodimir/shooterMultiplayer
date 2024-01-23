c2c267336bf6f8368798a81daf5133056e3014ef
MuliarVolodimir <mvolodya2007@gmail.com>
2024-01-23 19:10:23 +0200

    Inventoey Singletone

--- Assets/Scripts/Inventory/Slot.cs
+++ Assets/Scripts/Inventory/Slot.cs
@@ -9,26 +9,4 @@ public class Slot : MonoBehaviour
     public int itemCount;
     public bool isSelected;
     public Item item;
-
-    public void UpdateSlot(Item item, int itemCount)
-    {
-        this.item = item;
-        this.itemCount = itemCount;
-
-        if (item != null) _text.text = item.itemName + ":" + itemCount;
-        else _text.text = "";
-    }
-
-    public void SetSelected(bool selected)
-    {
-        if (selected)
-        {
-            this.GetComponent<Image>().color = Color.white;
-        }
-        else
-        {
-            this.GetComponent <Image>().color = Color.gray;
-        }
-        isSelected = selected;
-    }
 }
